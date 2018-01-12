96e81476b7f27896c600f9edb690110bdfa9439b
tg970 <tgoodman@outlook.com>
2018-01-12 15:00:48 -0600

    basic styling to toolie cards on languages page

--- public/js/lang.js
+++ public/js/lang.js
@@ -2,14 +2,14 @@ app.controller('LangController', [ '$http', '$route', '$scope', '$location', fun
   this.id = $scope.$parent.ctrl.lang
   this.searchterm = "";
   $http({
-    method: 'GET',
-    url: `http://localhost:3000/languages/${this.id}`
-  }).then(response => {
-    console.log('oneLang:',response.data);
-    this.lang = response.data;
-    this.tools = response.data.tools
-  }, error => {
-    console.error(error.message);
+      method: 'GET',
+      url: `http://localhost:3000/languages/${this.id}`
+    }).then(response => {
+      console.log('oneLang:',response.data);
+      this.lang = response.data;
+      this.tools = response.data.tools
+    }, error => {
+      console.error(error.message);
   }).catch(err => console.error('Catch', err));
 
   this.select = (id) => {
