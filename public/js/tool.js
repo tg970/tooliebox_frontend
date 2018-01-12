ceaf2da7d8a36861eb0774828cdaf109ba19d8d3
blakeharris <harrisblake85@gmail.com>
2018-01-12 14:15:37 -0700

    Tool updated with clickable tags, added css file

--- public/js/tool.js
+++ public/js/tool.js
@@ -1,18 +1,22 @@
-app.filter('replUrl', ['$sce', function ($sce) {
+app.filter('trustUrl', ['$sce', function ($sce) {
   return function(url) {
-    return $sce.trustAsResourceUrl(url+"?lite=true");
+    return $sce.trustAsResourceUrl(url);
   };
 }]);
 
 app.controller('ToolController', ['$http', '$scope', '$mdDialog', function($http, $scope, $mdDialog) {
-  this.lang = $scope.$parent.ctrl.lang
-  this.id = $scope.$parent.ctrl.tool
+  this.tool = {};
+  this.lang = $scope.$parent.ctrl.lang;
+  this.id = $scope.$parent.ctrl.tool;
+
   $http({
     method: 'GET',
     url: `http://localhost:3000/languages/${this.lang}/tools/${this.id}`
   }).then(response => {
     console.log('oneTool:',response.data);
-    this.tool = response.data
+    this.temp = response.data;
+    this.temp.repl_url += "?lite=true";
+    this.tool = this.temp;
   }, error => {
     console.error(error.message);
   }).catch(err => console.error('Catch', err));
