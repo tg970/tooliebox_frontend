7a99d040a22618dd445c4a757763b42d1107c850
tg970 <tgoodman@outlook.com>
2018-01-11 19:04:15 -0600

    public files created, app.js running and happy, partial for svg p.o.c

---
+++ public/js/app.js
@@ -0,0 +1,47 @@
+console.log('yep');
+const app = angular.module('toolieBox_app', ['ngRoute', 'ngMaterial']);
+
+app.controller('MainController', [
+  '$http', '$route', '$scope', '$location', '$mdDialog',
+  function($http, $route, $scope, $location, $mdDialog) {
+    let CtrlUrl = $location.url();
+    console.log('MainController:', CtrlUrl);
+    this.test = "hello"
+}]);
+
+app.config(['$routeProvider','$locationProvider', '$mdThemingProvider', function($routeProvider,$locationProvider, $mdThemingProvider) {
+  // Enables Push State
+  $locationProvider.html5Mode({ enabled: true });
+
+  $routeProvider.when('/', {
+    templateUrl: 'partials/home.html',
+    controller: 'MainController as ctrl',
+    controllerAs: 'ctrl'
+  });
+
+  // $routeProvider.when('/viewSystems', {
+  //   templateUrl: 'partials/system.html',
+  //   controller: 'SystemController as ctrl',
+  //   controllerAs: 'ctrl'
+  // });
+
+  $routeProvider.when('/about', {
+    templateUrl: 'partials/about.html',
+  });
+
+  // $routeProvider.when('/instructions', {
+  //   templateUrl: 'partials/instructions.html',
+  //   controller: 'MainController as ctrl',
+  //   controllerAs: 'ctrl'
+  // });
+
+  $routeProvider.otherwise({
+    redirectTo: '/'
+  });
+
+  $mdThemingProvider.theme('default')
+    .primaryPalette('blue-grey')
+    .accentPalette('blue')
+    .warnPalette('pink');
+
+}]);
