24f54aff259f1b3465269da7e00a48b07c8f5d1c
tg970 <tgoodman@outlook.com>
2018-01-15 13:07:29 -0600

    toggling repl url or link url field

--- public/js/createTool.js
+++ public/js/createTool.js
@@ -1,20 +1,25 @@
 app.controller('CreateController', [ '$http', '$route', '$scope', '$location', function($http, $route, $scope, $location) {
   console.log('CreateController');
   this.selectLang = $scope.$parent.ctrl.langs
+  this.repl = true
 
   this.submit = (newInfo) => {
     console.log('submit create form:', newInfo);
-    $http({
-        method: 'POST',
-        url: `http://localhost:3000/languages/${newInfo.language}/tools`,
-        data: newInfo
-      }).then(response => {
-        console.log('Post New Tool Response:',response.data);
-        $scope.$parent.ctrl.tool = response.data.id
-        $location.path('/toolie')
-      }, error => {
-        console.error(error.message);
-    }).catch(err => console.error('Catch', err));
+    // $http({
+    //     method: 'POST',
+    //     url: `http://localhost:3000/languages/${newInfo.language}/tools`,
+    //     data: newInfo
+    //   }).then(response => {
+    //     console.log('Post New Tool Response:',response.data);
+    //     $scope.$parent.ctrl.tool = response.data.id
+    //     $location.path('/toolie')
+    //   }, error => {
+    //     console.error(error.message);
+    // }).catch(err => console.error('Catch', err));
   };
+
+  this.toggleRepl = () => {
+    this.repl = !this.repl
+  }
 
 }]);
