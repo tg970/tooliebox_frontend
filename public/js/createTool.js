64946b001e7268668f56f81c46531d8edcb84ca2
tg970 <tgoodman@outlook.com>
2018-01-15 13:49:53 -0600

    post to api server working with new values

--- public/js/createTool.js
+++ public/js/createTool.js
@@ -5,21 +5,23 @@ app.controller('CreateController', [ '$http', '$route', '$scope', '$location', f
 
   this.submit = (newInfo) => {
     console.log('submit create form:', newInfo);
-    // $http({
-    //     method: 'POST',
-    //     url: `http://localhost:3000/languages/${newInfo.language}/tools`,
-    //     data: newInfo
-    //   }).then(response => {
-    //     console.log('Post New Tool Response:',response.data);
-    //     $scope.$parent.ctrl.tool = response.data.id
-    //     $location.path('/toolie')
-    //   }, error => {
-    //     console.error(error.message);
-    // }).catch(err => console.error('Catch', err));
+    $http({
+        method: 'POST',
+        url: `http://localhost:3000/languages/${newInfo.language}/tools`,
+        data: newInfo
+      }).then(response => {
+        console.log('Post New Tool Response:',response.data);
+        // $scope.$parent.ctrl.tool = response.data.id
+        // $location.path('/toolie')
+      }, error => {
+        console.error(error.message);
+    }).catch(err => console.error('Catch', err));
   };
 
   this.toggleRepl = () => {
     this.repl = !this.repl
+    delete $scope.newInfo.repl_url
+    delete $scope.newInfo.link_url
   }
 
 }]);
