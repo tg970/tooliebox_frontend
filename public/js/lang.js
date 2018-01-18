45b5ac015f87e861726afb7228b9e465f7cc2d14
tg970 <tgoodman@outlook.com>
2018-01-17 20:45:15 -0600

    getting closer to solution to remove from belt

--- public/js/lang.js
+++ public/js/lang.js
@@ -2,17 +2,28 @@ app.controller('LangController', [ '$http', '$route', '$scope', '$location', fun
   this.user = user;
   this.id = $scope.$parent.ctrl.lang.id
   this.searchterm = "";
-  $http({
-      method: 'GET',
-      url: `${api}/languages/${this.id}`
-    }).then(response => {
-      console.log('oneLang:',response.data);
-      this.lang = response.data;
-      this.tools = response.data.tools
-      this.numTools = this.tools.length
-    }, error => {
-      console.error(error.message);
-  }).catch(err => console.error('Catch', err));
+  this.getLang = () => {
+    $http({
+        method: 'GET',
+        url: `${api}/languages/${this.id}`
+      }).then(response => {
+        console.log('oneLang:',response.data);
+        this.lang = response.data;
+        this.tools = response.data.tools
+        console.log(this.tools);
+        if (user.logged) {
+          for (let tool of this.tools) {
+            if (user.arrTool.includes(tool.id)) {
+              tool.belted = true;
+            }
+          }
+        }
+        this.numTools = this.tools.length
+      }, error => {
+        console.error(error.message);
+    }).catch(err => console.error('Catch', err));
+  }
+  this.getLang()
 
   this.select = (id) => {
     $scope.$parent.ctrl.tool.id = id
@@ -40,11 +51,28 @@ app.controller('LangController', [ '$http', '$route', '$scope', '$location', fun
     }).catch(err => console.error('Catch', err));
   }
 
+  this.addToBelt = (id) => {
+    console.log('Add tool id:',id);
+    console.log('Add to user id', user.id);
+    let newInfo = { tool_id: id, user_id: user.id }
+    console.log('newInfo:', newInfo);
+    $http({
+        method: 'POST',
+        url: `${api}/toolbelts`,
+        data: newInfo
+      }).then(response => {
+        console.log('toolBelt response:',response.data);
+      }, error => {
+        console.error(error.message);
+    }).catch(err => console.error('Catch', err));
+  }
+
   //Listen for login
   $scope.$on('updateAuth', (data) => {
     // console.log('listener');
     this.user = user;
     this.user.logged = true;
+    this.getLang();
   })
 
 }]);
